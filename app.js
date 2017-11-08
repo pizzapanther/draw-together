3f85ffb0a841b4beb4afbe2c091da16ad6e0e926
Paul Bailey <paul.m.bailey@gmail.com>
2017-11-08 09:57:47 -0600

    canvas app

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.set('view engine', 'hbs');
+
+app.use('/socket-io',
+  express.static('node_modules/socket.io-client/dist'));
+
+app.get('/', function (request, response) {
+  response.render('draw.hbs');
+});
+
+app.get('/paint', function (request, response) {
+  response.render('paint.hbs');
+});
+
+io.on('connection', function(client){
+  console.log('CONNECTED');
+  
+  client.on('draw', function (coords) {
+    console.log('Coords', coords);
+    io.emit('do-draw', coords);
+  });
+});
+
+http.listen(8000, function () {
+  console.log('Listening on port 8000');
+});
+
